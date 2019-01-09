0549620812e2bf0369bd1ed53daa4dd5294f5bd6
KedamaOvO <moe@mao-yu.net>
2019-01-09 22:45:40 +0800

    custom style

--- src/getCss.ts
+++ src/getCss.ts
@@ -1,6 +1,22 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as vscode from 'vscode';
+
+function getStyleByOptions(styleOption: any): string {
+    let styleArr: string[] = [];
+    for (let k in styleOption) {
+        // 在使用背景图时，排除掉 pointer-events
+        if (~['pointer-events', 'z-index'].indexOf(k)) {
+            continue;
+        }
+
+        if (styleOption.hasOwnProperty(k)) {
+            styleArr.push(`${k}:${styleOption[k]}`);
+        }
+    }
+    return styleArr.join(';');
+}
+
 export default function(config:any,extName:string,version:string):string{
 	let opacity:number = config.opacity;
 	let imagePath:string;
@@ -18,11 +34,13 @@ export default function(config:any,extName:string,version:string):string{
 	opacity = opacity<=0.1?0.1:opacity>=1?1:opacity;
 	opacity = 0.89 + (0.1-(opacity / 10));
 	
+	const cssString = getStyleByOptions(config.style);
+
 	return `
 	/*ext-${extName}-start*/
 	/*ext.${extName}.ver.${version}*/
 	body{
-		background-size:cover;
+		${cssString};
 		opacity:${opacity};
 		background-image:url('${imagePath}');
 	}
