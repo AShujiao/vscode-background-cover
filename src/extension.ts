a9ce420bb57f18f840cb1787de783a5482b473d5
manasxx <916817706@qq.com>
2019-04-08 17:48:46 +0800

    todo

--- src/extension.ts
+++ src/extension.ts
@@ -13,3 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
+
+/*
+ * 选择图片
+    * 手动选择
+        * 弹出文件选择框
+    * 随机选择
+    * img1
+    * img2
+    * img3
+ * 添加目录
+    * 弹出文件目录选择框
+ * 透明度
+    * 输入数值（0.00-1.00）
+ * 手动输入/https
+    * 输入图片地址支持本地路径、https路径
+ * 关闭/开启背景
+ * 
+*/
